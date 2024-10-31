53f83dbe3f2668fb4511cdf0bc9a33e6068bf74c
abdulaziz <a.xazratqulov@1it.uz>
2024-10-31 14:59:53 +0500

    create project

---
+++ src/utils/env/index.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const DB_PORT = +process.env.DB_PORT || 5432;
+const DB_HOST = process.env.DB_HOST || '';
+const DB_USER = process.env.DB_USERNAME || '';
+const DB_DB = process.env.DB_DATABASE || '';
+const DB_PASS = process.env.DB_PASSWORD || '';
+
+
+export {
+    DB_PORT,
+    DB_HOST,
+    DB_USER,
+    DB_DB,
+    DB_PASS,
+};
